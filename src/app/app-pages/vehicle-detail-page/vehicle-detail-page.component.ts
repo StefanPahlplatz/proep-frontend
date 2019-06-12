0a032b45f1fb078916a0ea982fd4b93355ec03fc
StefanPahlplatz <stefanpahlplatz@inogo.nl>
2019-06-12 15:09:21 +0200

    rip

--- src/app/app-pages/vehicle-detail-page/vehicle-detail-page.component.ts
+++ src/app/app-pages/vehicle-detail-page/vehicle-detail-page.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core'
 import { VehicleViewModel } from 'app/models/view-models/vehicle-view-model'
+import { FormControl, FormGroup } from '@angular/forms'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { environment } from '../../../environments/environment'
+import { UserService } from '../../services/user.service'
+import { AuthService } from '../../services/auth.service'
+import { catchError } from 'rxjs/operators'
+import { of } from 'rxjs/internal/observable/of'
+import { UserDto } from '../../models/dtos/user-dto'
+import { ActivatedRoute, Params } from '@angular/router'
 
 @Component({
   selector: 'app-vehicle-detail-page',
@@ -9,8 +18,17 @@ import { VehicleViewModel } from 'app/models/view-models/vehicle-view-model'
 export class VehicleDetailPageComponent implements OnInit {
   location: string = 'Amsterdam'
   vehicle: VehicleViewModel
+  formdata: FormGroup
+  isLoading: boolean
+  isLoggedIn: boolean
+  user: any
+  vehicleId: string
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.vehicle = {
       id: 123456789,
       brand: 'Tesla',
@@ -18,7 +36,34 @@ export class VehicleDetailPageComponent implements OnInit {
       model: 'Model S',
       vehicleType: 'Luxury',
     }
+    this.isLoading = false
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.formdata = new FormGroup({
+      fromDateInput: new FormControl(''),
+      tillDateInput: new FormControl(''),
+    })
+    this.isLoggedIn = this.authService.isAuthenticated()
+    this.authService.getCurrentUser().subscribe((user: UserDto) => {
+      this.user = user
+    })
+    this.activatedRoute.queryParams.subscribe((queryparam: Params) => {
+      console.log(queryparam)
+    })
+  }
+
+  onClickSubmit(data) {
+    console.log(data)
+    if (data.fromDateInput && data.tillDateInput) {
+      console.log('submit')
+      this.http.post(`${environment.airRnD.baseUrl}/reservation/`, {
+        userId: this.user.id,
+      })
+      this.isLoading = true
+      setTimeout(() => {
+        this.isLoading = false
+      }, 1400)
+    }
+  }
 }
